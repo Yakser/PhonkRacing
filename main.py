e49597caf640758ffe244cfcb2ac96c3e476cd25
Sergey Yaksanov <iaksanov2044@mail.ru>
2020-12-26 21:15:30 +0400

    Счетчик пройденного расстояния

--- main.py
+++ main.py
@@ -155,7 +155,6 @@ if __name__ == '__main__':
 
     while running:
         screen.fill((0, 0, 0))
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
